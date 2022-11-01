7a77b597786c2748d962e06b1a8c8d57d9667e0e
Yanov Bulat <yanovwork@gmail.com>
2022-11-01 12:18:15 +0300

    step 9.14

--- src/Games/brain-gcd-game.php
+++ src/Games/brain-gcd-game.php
@@ -2,11 +2,26 @@
 
 namespace Src\Games\Brain\Gcd\Game;
 
+#функция которая определяет наибольший общий делитель
+function greatestCommonDivisorOfTwoNumbers(int $firstAcceptedNumber, int $secondAcceptedNumber): int
+{
+    $a = abs($firstAcceptedNumber);
+    $b = abs($secondAcceptedNumber);
+    while ($a != $b) {
+        if ($a > $b) {
+            $a -= $b;
+        } else {
+            $b -= $a;
+        }
+    }
+    return $a;
+}
+
 #функция которая определяет правильно ли ответил пользователь
 function userResponseGCD($accepsedUserResponce, int $firstAcceptedNumber, int $secondAcceptedNumber): bool
 {
     $intAccepsedUserResponce = (int)$accepsedUserResponce;
-    if ($intAccepsedUserResponce == gmp_gcd($firstAcceptedNumber, $secondAcceptedNumber)) {
+    if ($intAccepsedUserResponce == greatestCommonDivisorOfTwoNumbers($firstAcceptedNumber, $secondAcceptedNumber)) {
         return true;
     } else {
         return false;
