5446e42e0c577c0909233f9bf949899c9a00053e
Yanov Bulat <yanovwork@gmail.com>
2022-11-16 16:31:28 +0300

    after review 1.8

--- src/Games/BrainPrimeGame.php
+++ src/Games/BrainPrimeGame.php
@@ -6,36 +6,33 @@ use function src\Engine\runGame;
 
 const TASK = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-function definesPrimeNumber(int $acceptedNumber): string
+function definesPrimeNumber(int $acceptedNumber): bool
 {
     for ($i = 2; $i < $acceptedNumber; $i++) {
         if ($acceptedNumber % $i == 0) {
-            return 'no';
+            return false;
         }
     }
-    return 'yes';
+    return true;
 }
 
 function runIsItPrimeNumber()
 {
     require_once __DIR__ . '/../Engine.php';
-    $numberAndAnswer = [];
+    $questions = [];
+    $answers = [];
+    $randArray = [];
     for ($i = 0; $i < 3; $i++) {
-        $number = rand(1, 99);
-        switch (mt_rand(1, 3)) {
-            case 1:
-                $rightResponse = determineParity($number);
-                $numbersAndAnswer["$number"] = $rightResponse;
-                break;
-            case 2:
-                $rightResponse = determineParity($number);
-                $numbersAndAnswer["$number"] = $rightResponse;
-                break;
-            case 3:
-                $rightResponse = determineParity($number);
-                $numbersAndAnswer["$number"] = $rightResponse;
-                break;
+        $randArray[] = rand(1, 99);
+    }
+    foreach ($randArray as $number) {
+        $questions[] = $number;
+        if (definesPrimeNumber($number) == true) {
+            $answers[] = 'yes';
+        } else {
+            $answers[] = 'no';
         }
     }
+    $numbersAndAnswer = array_combine($questions, $answers);
     runGame(TASK, $numbersAndAnswer);
 }
